1928bb592e289becbe8be0ac132a64878fa35b76
prioritize <jared@prototypic.al>
2024-12-05 11:02:42 -0800

    progress

--- src/day_2_redo.rs
+++ src/day_2_redo.rs
@@ -1,21 +1,79 @@
-fn less(a: &u32, b: &u32, delta: u32) -> bool {
-    (a < b) && ((b - a) < delta) && !(a == b)
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub fn less(a: &u32, b: &u32, delta: u32) -> bool {
+    (a < b) && ((b - a) < delta) && (a != b)
 }
-fn more(a: &u32, b: &u32, delta: u32) -> bool {
-    (a > b) && (a - b) < delta && !(a == b)
+pub fn more(a: &u32, b: &u32, delta: u32) -> bool {
+    (a > b) && (a - b) < delta && (a != b)
 }
-fn eval(report: &[u32]) -> bool {
+pub fn eval(report: &[u32]) -> bool {
     let mut iter = report.iter().peekable();
+    let mut more_check = true;
     while let Some(v) = iter.next() {
         if let Some(n) = iter.peek() {
-            if more(v, n, 3) || less(v, n, 3) {
+            if !more_check {
+                break;
+            }
+            if more(v, n, 4) {
                 continue;
             } else {
-                return false;
+                more_check = false;
             }
         }
     }
-    true
+
+    let mut less_check = true;
+    let mut iter = report.iter().peekable();
+    while let Some(v) = iter.next() {
+        if let Some(n) = iter.peek() {
+            if !less_check {
+                break;
+            }
+            if less(v, n, 4) {
+                continue;
+            } else {
+                less_check = false;
+            }
+        }
+    }
+    more_check || less_check
+}
+pub fn check_all_reports(reports: Vec<Vec<u32>>, dampen: bool) -> u32 {
+    reports
+        .iter()
+        .map(|report| {
+            let out = eval(report);
+            if out {
+                println!("{:?} -- SAFE", report);
+            } else {
+                println!("{:?} -- UNSAFE", report);
+            }
+            out
+        })
+        .collect::<Vec<bool>>()
+        .iter()
+        .map(|r| if *r { 1 } else { 0 })
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum()
+}
+pub fn read_file_to_line(fname: &str) -> Vec<Vec<u32>> {
+    let file = File::open(fname).unwrap_or_else(|_| panic!("Unable to open {}", fname));
+    let lines = BufReader::new(file).lines();
+    lines
+        .map(|l| {
+            let mut temp_vec = Vec::new();
+            let line = l.unwrap();
+            let split = line.split_whitespace();
+            for value in split {
+                temp_vec.push(value.parse::<u32>().unwrap());
+            }
+            temp_vec
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -40,5 +98,15 @@ mod tests {
     fn test_eval_fail_two() {
         let test_val = vec![1, 10, 3, 4, 5, 6, 7, 8];
         assert!(!eval(&test_val))
+    }
+    #[test]
+    fn test_example_input() {
+        let reports = read_file_to_line("input/day_2_test_input.txt");
+        assert!(2 == check_all_reports(reports, false));
+    }
+    #[test]
+    fn test_input() {
+        let reports = read_file_to_line("input/day_2_input.txt");
+        assert_eq!(564, check_all_reports(reports, false));
     }
 }
