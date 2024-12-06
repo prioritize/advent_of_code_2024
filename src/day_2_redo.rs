52d1f4a47d076bd83498462a40a5bffcda3ca218
prioritize <jared@prototypic.al>
2024-12-05 18:22:44 -0800

    progress

--- src/day_2_redo.rs
+++ src/day_2_redo.rs
@@ -3,62 +3,57 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+#[inline]
+pub fn compare(a: &u32, b: &u32, delta: u32, func: fn(&u32, &u32, delta: u32) -> bool) -> bool {
+    func(a, b, delta)
+}
 pub fn less(a: &u32, b: &u32, delta: u32) -> bool {
     (a < b) && ((b - a) < delta) && (a != b)
 }
 pub fn more(a: &u32, b: &u32, delta: u32) -> bool {
     (a > b) && (a - b) < delta && (a != b)
 }
-pub fn eval(report: &[u32]) -> bool {
-    let mut iter = report.iter().peekable();
-    let mut more_check = true;
-    while let Some(v) = iter.next() {
-        if let Some(n) = iter.peek() {
-            if !more_check {
-                break;
-            }
-            if more(v, n, 4) {
-                continue;
-            } else {
-                more_check = false;
-            }
-        }
-    }
-
-    let mut less_check = true;
+pub fn eval(report: &[u32], func: fn(&u32, &u32, u32) -> bool) -> bool {
     let mut iter = report.iter().peekable();
     while let Some(v) = iter.next() {
         if let Some(n) = iter.peek() {
-            if !less_check {
-                break;
-            }
-            if less(v, n, 4) {
+            if func(v, n, 4) {
                 continue;
             } else {
-                less_check = false;
+                return false;
             }
         }
     }
-    more_check || less_check
+    true
 }
+pub fn eval_dampen(report: &[u32], func: fn(&u32, &u32, u32) -> bool) -> bool {
+    let mut skipped = false;
+    for (idx, val) in report.iter().enumerate() {}
+    true
+}
+
 pub fn check_all_reports(reports: Vec<Vec<u32>>, dampen: bool) -> u32 {
-    reports
-        .iter()
-        .map(|report| {
-            let out = eval(report);
-            if out {
-                println!("{:?} -- SAFE", report);
-            } else {
-                println!("{:?} -- UNSAFE", report);
-            }
-            out
-        })
-        .collect::<Vec<bool>>()
-        .iter()
-        .map(|r| if *r { 1 } else { 0 })
-        .collect::<Vec<u32>>()
-        .iter()
-        .sum()
+    if dampen {
+        reports
+            .iter()
+            .map(|report| {
+                let out = eval(report, more) || eval(report, less);
+                if out {
+                    println!("{:?} -- SAFE", report);
+                } else {
+                    println!("{:?} -- UNSAFE", report);
+                }
+                out
+            })
+            .collect::<Vec<bool>>()
+            .iter()
+            .map(|r| if *r { 1 } else { 0 })
+            .collect::<Vec<u32>>()
+            .iter()
+            .sum()
+    } else {
+        0
+    }
 }
 pub fn read_file_to_line(fname: &str) -> Vec<Vec<u32>> {
     let file = File::open(fname).unwrap_or_else(|_| panic!("Unable to open {}", fname));
@@ -82,22 +77,22 @@ mod tests {
     #[test]
     fn test_eval() {
         let test_val = vec![1, 2, 3, 4, 5, 6, 7, 8];
-        assert!(eval(&test_val))
+        assert!(eval(&test_val, less))
     }
     #[test]
     fn test_eval_fail() {
         let test_val = vec![1, 1, 3, 4, 5, 6, 7, 8];
-        assert!(!eval(&test_val))
+        assert!(!eval(&test_val, more))
     }
     #[test]
     fn test_eval_fail_one() {
         let test_val = vec![1, 0, 3, 4, 5, 6, 7, 8];
-        assert!(!eval(&test_val))
+        assert!(!eval(&test_val, more))
     }
     #[test]
     fn test_eval_fail_two() {
         let test_val = vec![1, 10, 3, 4, 5, 6, 7, 8];
-        assert!(!eval(&test_val))
+        assert!(!eval(&test_val, more))
     }
     #[test]
     fn test_example_input() {
