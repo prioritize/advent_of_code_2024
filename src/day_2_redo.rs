6e3410885963c895de1599aa3b38a875b9db59e3
prioritize <jared@prototypic.al>
2024-12-09 18:33:54 -0800

    progress

--- src/day_2_redo.rs
+++ src/day_2_redo.rs
@@ -1,3 +1,4 @@
+use color_eyre::Report;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
@@ -26,15 +27,60 @@ pub fn eval(report: &[u32], func: fn(&u32, &u32, u32) -> bool) -> bool {
     }
     true
 }
+pub fn delta_difference(report: &[u32]) -> Vec<(i32, i32)> {
+    report
+        .iter()
+        .enumerate()
+        .map(|(idx, val)| match idx < report.len() - 1 {
+            true => {
+                let neighbor = report[idx + 1] as i32;
+                let val = *val as i32;
+                Some(((val - neighbor), (neighbor - val)))
+            }
+            false => None,
+        })
+        .collect::<Vec<Option<(i32, i32)>>>()
+        .into_iter()
+        .flatten()
+        .collect()
+}
+pub fn eval_delta(deltas: Vec<(i32, i32)>, max_delta: i32, dampen: bool) -> u32 {
+    let checks: Vec<bool> = deltas
+        .iter()
+        .enumerate()
+        .map(|(idx, (lesser, greater))| {
+            (!lesser.is_negative() && *lesser < max_delta)
+                || (!greater.is_negative() && *greater < max_delta)
+        })
+        .collect();
+    println!("{:?}", checks);
+    let test_val = checks
+        .into_iter()
+        .filter(|x| *x)
+        .collect::<Vec<bool>>()
+        .iter()
+        .len() as u32;
+    println!("{}", test_val);
+    todo!()
+}
+
 pub fn eval_dampen(report: &[u32], func: fn(&u32, &u32, u32) -> bool) -> bool {
+    println!("incoming data: {:?}", report);
     for (idx, val) in report.iter().enumerate() {
         if idx + 1 < report.len() {
             if func(val, &report[idx + 1], 4) {
-                return true;
+                continue;
             } else if idx + 2 < report.len() {
                 let temp = &[&report[idx..idx + 1], &report[(idx + 2)..]].concat();
+                println!("Temp report is: {:?}", temp);
                 return eval(temp, func);
             } else {
+                if eval(&report[1..], func) {
+                    return true;
+                }
+                if eval(&report[..=(report.len() - 2)], func) {
+                    return true;
+                }
                 return false;
             }
         }
@@ -49,7 +95,6 @@ pub fn check_all_reports(reports: Vec<Vec<u32>>, dampen: bool) -> u32 {
             .map(|report| {
                 let out = eval(report, more) || eval(report, less);
                 if out {
-                    println!("{:?} -- SAFE", report);
                 } else {
                     println!("{:?} -- UNSAFE", report);
                 }
@@ -66,8 +111,8 @@ pub fn check_all_reports(reports: Vec<Vec<u32>>, dampen: bool) -> u32 {
             .iter()
             .map(|report| {
                 let out = eval_dampen(report, more) || eval_dampen(report, less);
+                println!("results of eval_dampen: {}", out);
                 if out {
-                    println!("{:?} -- SAFE", report);
                 } else {
                     println!("{:?} -- UNSAFE", report);
                 }
@@ -95,6 +140,11 @@ pub fn read_file_to_line(fname: &str) -> Vec<Vec<u32>> {
             temp_vec
         })
         .collect()
+}
+
+pub fn check_all_reports_delta(reports: Vec<Vec<u32>>) -> u32 {
+    let deltas: Vec<Vec<(i32, i32)>> = reports.iter().map(|r| delta_difference(r)).collect();
+    deltas.iter().map(|d| eval_delta(d, 4, false))
 }
 
 #[cfg(test)]
